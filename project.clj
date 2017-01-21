e157d2eb9d8949d7b7eab874de49f7486d0dd6d1
Stephen Sloan <steve@stevesloan.com>
2017-01-20 19:11:20 -0800

    Version 0.1.5c; Deps cleanup

--- project.clj
+++ project.clj
@@ -1,18 +1,19 @@
 (def schema-tools-version "0.9.0")
 (def schema-version "1.1.3")
 
-(defproject threatgrid/flanders "0.1.5b"
+(defproject threatgrid/flanders "0.1.5c"
   :description "flanders"
   :url "http://github.com/threatgrid/flanders"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojure-future-spec "1.9.0-alpha14"]
-                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojure/core.match "0.3.0-alpha4"
+                  :exclusions [org.clojure/tools.reader]]
                  [cheshire "5.6.3"]
 
                  [prismatic/schema ~schema-version]
-                 [metosin/ring-swagger "0.22.9"]
+                 [metosin/ring-swagger "0.22.11"]
                  [metosin/schema-tools ~schema-tools-version]]
   :profiles {:dev
              {:dependencies [[org.clojure/test.check "0.9.0"]]}})
