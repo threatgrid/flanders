51d4c93c1d41ff50a5ab594816add03a6d019724
Paula Gearon <pgearon@cisco.com>
2018-01-10 11:07:13 -0500

    Updated to Clojure 1.9

--- project.clj
+++ project.clj
@@ -1,19 +1,18 @@
-(def schema-tools-version "0.9.0")
-(def schema-version "1.1.3")
+(def schema-tools-version "0.9.1")
+(def schema-version "1.1.7")
 
 (defproject threatgrid/flanders "0.1.14-SNAPSHOT"
   :description "flanders"
   :url "http://github.com/threatgrid/flanders"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clojure-future-spec "1.9.0-alpha14"]
-                 [org.clojure/core.match "0.3.0-alpha4"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.match "0.3.0-alpha5"
                   :exclusions [org.clojure/tools.reader]]
-                 [cheshire "5.6.3"]
+                 [cheshire "5.8.0"]
 
                  [prismatic/schema ~schema-version]
-                 [metosin/ring-swagger "0.22.11"]
+                 [metosin/ring-swagger "0.24.4"]
                  [metosin/schema-tools ~schema-tools-version]]
   :profiles {:dev
              {:dependencies [[org.clojure/test.check "0.9.0"]]}})
