dfc855f4e327b98640a0760ff1224615989c2b80
Joel Holdbrooks <cjholdbrooks@gmail.com>
2019-09-16 13:25:17 -0700

    Add markdown test suite

---
+++ test/flanders/markdown_test.clj
@@ -0,0 +1,24 @@
+(ns flanders.markdown-test
+  (:require [clojure.test :refer [deftest is]]
+            [flanders.core :as f]
+            [flanders.markdown :as f.markdown]))
+
+
+(deftest signuature-type->markdown
+  ;; TODO: This causes an error.
+  (is (= (f.markdown/->markdown (f/sig :parameters []))
+         "() => Anything\n\n"))
+
+  (is (= (f.markdown/->markdown (f/sig :parameters [(f/int)]))
+         "(Integer) => Anything\n\n"))
+
+  (is (= (f.markdown/->markdown (f/sig :parameters [(f/int) (f/str)]))
+         "(Integer,  String) => Anything\n\n\n"))
+
+  (is (= (f.markdown/->markdown (f/sig :parameters [(f/int)]
+                                       :rest-parameter (f/str)))
+         "(Integer,  String...) => Anything\n\n\n"))
+
+  (is (= (f.markdown/->markdown (f/sig :parameters [(f/int)]
+                                       :rest-parameter (f/str)))
+         "(Integer,  String...) => Anything\n\n\n")))
