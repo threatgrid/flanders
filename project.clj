1a3ac64e944007591b498f1c32e5b79f57610177
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-01-30 10:37:14 -0500

    bump deps + pin CI to openjdk11

--- project.clj
+++ project.clj
@@ -1,18 +1,17 @@
-(def schema-tools-version "0.9.1")
-(def schema-version "1.1.7")
+(def schema-tools-version "0.12.1")
+(def schema-version "1.1.12")
 
 (defproject threatgrid/flanders "0.1.23-SNAPSHOT"
   :description "flanders"
   :url "http://github.com/threatgrid/flanders"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.match "0.3.0-alpha5"
-                  :exclusions [org.clojure/tools.reader]]
-                 [cheshire "5.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.match "0.3.0"]
+                 [cheshire "5.9.0"]
 
                  [prismatic/schema ~schema-version]
-                 [metosin/ring-swagger "0.24.4"]
+                 [metosin/ring-swagger "0.26.2"]
                  [metosin/schema-tools ~schema-tools-version]]
   :profiles {:dev
-             {:dependencies [[org.clojure/test.check "0.9.0"]]}})
+             {:dependencies [[org.clojure/test.check "0.10.0"]]}})
