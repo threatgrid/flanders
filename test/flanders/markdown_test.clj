382c4f998ca9962b574a972fc909ab21b2e33c37
vemv <vemv@users.noreply.github.com>
2021-04-19 14:35:31 +0200

    Adapt a test

--- test/flanders/markdown_test.clj
+++ test/flanders/markdown_test.clj
@@ -4,25 +4,53 @@
             [flanders.markdown :as f.markdown]))
 
 (deftest signature-type->markdown
-  (is (= "### Signature\n\n() => Anything\n\n\n"
-         (f.markdown/->markdown (f/sig :parameters []))))
+  (is (= "### Signature\n\n() => Anything\n\n"
+         (-> :parameters
+             (f/sig [])
+             f.markdown/->markdown
+             first
+             second)))
 
-  (is (= "### Signature\n\n(Integer) => Anything\n\n\n\n"
-         (f.markdown/->markdown (f/sig :parameters [(f/int)]))))
+  (is (= "### Signature\n\n(Integer) => Anything\n\n"
+         (-> :parameters
+             (f/sig [(f/int)])
+             f.markdown/->markdown
+             first
+             second)))
 
-  (is (= "### Signature\n\n(Integer, String) => Anything\n\n\n\n\n"
-         (f.markdown/->markdown (f/sig :parameters [(f/int) (f/str)]))))
+  (is (= "### Signature\n\n(Integer, String) => Anything\n\n"
+         (-> :parameters
+             (f/sig [(f/int) (f/str)])
+             f.markdown/->markdown
+             first
+             second)))
 
-  (is (= "### Signature\n\n(Integer, String, Integer, String) => Anything\n\n\n\n\n\n\n"
-         (f.markdown/->markdown (f/sig :parameters [(f/int) (f/str) (f/int) (f/str)]))))
+  (is (= "### Signature\n\n(Integer, String, Integer, String) => Anything\n\n"
+         (-> :parameters
+             (f/sig [(f/int) (f/str) (f/int) (f/str)])
+             f.markdown/->markdown
+             first
+             second)))
 
-  (is (= "### Signature\n\n(Integer, String ...) => Anything\n\n\n\n\n"
-         (f.markdown/->markdown (f/sig :parameters [(f/int)] :rest-parameter (f/str)))))
+  (is (= "### Signature\n\n(Integer, String ...) => Anything\n\n"
+         (-> :parameters
+             (f/sig [(f/int)] :rest-parameter (f/str))
+             f.markdown/->markdown
+             first
+             second)))
 
-  (is (= "### Signature\n\n(Integer, String ...) => Anything\n\n\n\n\n"
-         (f.markdown/->markdown (f/sig :parameters [(f/int)] :rest-parameter (f/str)))))
+  (is (= "### Signature\n\n(Integer, String ...) => Anything\n\n"
+         (-> :parameters
+             (f/sig [(f/int)] :rest-parameter (f/str))
+             f.markdown/->markdown
+             first
+             second)))
 
-  (is (= "# `Foo`\n\n### Description\n\nThe Foo.\n\n### Signature\n\n() => Anything\n\n\n"
-         (f.markdown/->markdown (f/sig :name "Foo"
-                                       :description "The Foo."
-                                       :parameters [])))))
+  (is (= "# `Foo`\n\n### Description\n\nThe Foo.\n\n### Signature\n\n() => Anything\n\n"
+         (-> :name
+             (f/sig "Foo"
+                    :description "The Foo."
+                    :parameters [])
+             f.markdown/->markdown
+             first
+             second))))
