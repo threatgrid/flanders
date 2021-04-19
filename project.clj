6f77883e93d4393271ecd11156d1f0b107ef9a73
vemv <vemv@users.noreply.github.com>
2021-04-19 14:10:32 +0200

    Cleanup CI setup

--- project.clj
+++ project.clj
@@ -1,16 +1,14 @@
 (defproject threatgrid/flanders "0.1.24-SNAPSHOT"
   :description "flanders"
-  :url "http://github.com/threatgrid/flanders"
+  :url "https://github.com/threatgrid/flanders"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :pedantic? :abort
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.clojure/core.match "1.0.0"]
                  [cheshire "5.9.0"]
-
                  [prismatic/schema "1.1.12"]
                  [metosin/ring-swagger "0.26.2"]
                  [metosin/schema-tools "0.12.2"]]
-  :global-vars {*warn-on-reflection* true}
-  :profiles {:dev
-             {:dependencies [[org.clojure/test.check "1.0.0"]]}})
+  :profiles {:test {:dependencies [[org.clojure/test.check "1.0.0"]]}
+             :ci {:pedantic? :abort
+                  :global-vars {*warn-on-reflection* true}}})
