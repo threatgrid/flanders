858f4db4520996aae523012e542d9b5e558422b7
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2023-12-19 11:21:32 -0600

    make test.check a runtime dep

--- project.clj
+++ project.clj
@@ -3,9 +3,14 @@
   :url "http://github.com/threatgrid/flanders"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[metosin/ring-swagger "0.26.2"]
-                 [metosin/schema-tools "0.13.1"]
-                 [org.clojure/clojure "1.10.1"]
+  :pedantic? :abort
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.match "1.0.0"]
-                 [org.clojure/test.check "1.1.1"]
-                 [prismatic/schema "1.4.1"]])
+                 [cheshire "5.9.0"]
+
+                 [prismatic/schema "1.1.12"]
+                 [metosin/ring-swagger "0.26.2"]
+                 [metosin/schema-tools "0.12.2"]]
+  :global-vars {*warn-on-reflection* true}
+  :profiles {:dev
+             {:dependencies [[org.clojure/test.check "1.0.0"]]}})
