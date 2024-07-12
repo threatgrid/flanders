049998e4c55915d3b76f7cffeb9f6337c1c6edc0
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2024-07-12 13:00:02 -0500

    fix

--- test/flanders/markdown_test.clj
+++ test/flanders/markdown_test.clj
@@ -1,8 +1,18 @@
 (ns flanders.markdown-test
   (:require
+   [clojure.string :as str]
    [clojure.test :refer [deftest is]]
    [flanders.core :as f]
    [flanders.markdown :as md]))
+
+(deftest ready-for-table-test
+  (is (= " \\\\|" (md/ready-for-table "\n|"))))
+
+(f/def-entity-type Actor
+  {}
+  (f/optional-entries
+   (f/entry :confidence f/any
+            :description "separated\nby\nspaces within table")))
 
 (deftest signature-type->markdown
   (is (= "### Signature\n\n() => Anything\n\n\n"
@@ -26,4 +36,23 @@
   (is (= "# `Foo`\n\n### Description\n\nThe Foo.\n\n### Signature\n\n() => Anything\n\n\n"
          (md/->markdown (f/sig :name "Foo"
                                :description "The Foo."
-                               :parameters [])))))
+                               :parameters []))))
+
+  (is (= ["<a id=\"top\"></a>"
+          "# *Actor* Object"
+          ""
+          "| Property | Type | Description | Required? |"
+          "| -------- | ---- | ----------- | --------- |"
+          "|[confidence](#propertyconfidence-anything)|Anything|separated by spaces within table||"
+          ""
+          ""
+          "<a id=\"propertyconfidence-anything\"></a>"
+          "## Property confidence ∷ Anything"
+          ""
+          "separated"
+          "by"
+          "spaces within table"
+          ""
+          "* This entry is optional"]
+         (-> (md/->markdown Actor)
+             str/split-lines))))
