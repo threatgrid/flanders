cc8cca63b94ced43037bec788b6b9c319e9607c0
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2024-07-16 15:04:46 -0500

    Version 1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/flanders "1.0.2-SNAPSHOT"
+(defproject threatgrid/flanders "1.0.2"
   :description "flanders"
   :url "http://github.com/threatgrid/flanders"
   :license {:name "Eclipse Public License"
