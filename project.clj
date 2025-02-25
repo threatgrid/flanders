9b1779542b71379ade6914b588114d7c0829a84d
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2025-02-25 13:25:30 -0600

    revert

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :pedantic? :abort
-  :dependencies [[org.clojure/clojure "1.11.4"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/core.match "1.0.0"]
                  [prismatic/schema "1.2.0"]
                  [metosin/ring-swagger "1.0.0"]
